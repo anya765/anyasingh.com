f9bcbe327e948631fb68d6d8d15deaf79e9ea979
anya s <anyasingh070605@gmail.com>
2022-11-07 22:48:54 -0500

    new commit

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/favicon-32x32.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
