62a4e222b609b04eb06f0800d5f0c02cd8038477
Anya Singh <anyasingh070605@gmail.com>
2023-02-09 22:49:23 -0800

    fixed link

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,7 @@ export default function Home({ posts }) {
             </h1>
             <p>
               I spend most of my time working on hard problems and trying to <br /> understand the
-              world around me. At the moment, I'm building
-              <Link href="https://anyasingh.notion.site/discovering-cost-effective-catalysts-for-renewable-energy-ba151fdabab64d289680d88ac57d49e4">
-                <a>
-                  {' '}
-                  <mark>climate</mark>
-                </a>
-              </Link>
+              world around me. At the moment, I'm building climate
               <br /> solutions with AI, working on blockchain research, and generally <br />{' '}
               exploring{' '}
               <Link href="https://selfsupervisedlearning.substack.com">
