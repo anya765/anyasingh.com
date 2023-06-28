47a5032a7a2659d276cd73406e7e9d827de56d1d
Anya Singh <anyasingh070605@gmail.com>
2023-06-28 00:49:40 -0700

    new

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: ['eslint:recommended', 'next', 'next/core-web-vitals'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
