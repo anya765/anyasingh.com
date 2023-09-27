71227c758534f76d15aa75b26f6cf6b0e1070d12
Anya Singh <anyasingh070605@gmail.com>
2023-09-27 15:26:22 -0400

    removed italics

--- pages/bookshelf.js
+++ pages/bookshelf.js
@@ -27,72 +27,41 @@ export default function Bookshelf() {
           <br />
           <ul>
             <li>
-              <i>
-                <a href="http://paulgraham.com/greatwork.html"> how to do great work</a>
-              </i>
+              <a href="http://paulgraham.com/greatwork.html"> how to do great work</a>
             </li>
             <li>
-              <i>
-                <a href="https://www.youtube.com/watch?v=PUv66718DII">inventing on principle </a>
-              </i>{' '}
+              <a href="https://www.youtube.com/watch?v=PUv66718DII">inventing on principle </a>{' '}
             </li>
             <li>
-              <i>
-                <a href="https://slatestarcodex.com/2014/07/30/meditations-on-moloch/">
-                  meditations on moloch
-                </a>
-              </i>
+              <a href="https://slatestarcodex.com/2014/07/30/meditations-on-moloch/">
+                meditations on moloch
+              </a>
             </li>
             <li>
-              <i>
-                <a href="https://www.benkuhn.net/outliers/">searching for outliers</a>
-              </i>{' '}
+              <a href="https://www.benkuhn.net/outliers/">searching for outliers</a>{' '}
+            </li>
+            <li>salt: a world history </li>
+            <li>a collection of poems by robert frost </li>
+            <li>building scientific apparatus </li>
+            <li>why nations fail </li>
+            <li>bell labs - idea factory </li>
+            <li>the design of everyday things </li>
+            <li>the art and science of doing engineering </li>
+            <li>invention on principle </li>
+            <li>a mathematician's apology </li>
+            <li>guns, germs and steel </li>
+            <li>
+              <a href="http://www.incompleteideas.net/IncIdeas/BitterLesson.html">
+                the bitter lesson
+              </a>{' '}
             </li>
             <li>
-              <i>salt: a world history</i>{' '}
+              <a href="https://www.ben-evans.com/benedictevans/2020/5/16/not-even-wrong">
+                predicting tech
+              </a>{' '}
             </li>
             <li>
-              <i>a collection of poems by robert frost</i>{' '}
-            </li>
-            <li>
-              <i>why nations fail</i>{' '}
-            </li>
-            <li>
-              <i>bell labs - idea factory</i>{' '}
-            </li>
-            <li>
-              <i>the design of everyday things</i>{' '}
-            </li>
-            <li>
-              <i>the art and science of doing engineering</i>{' '}
-            </li>
-            <li>
-              <i>invention on principle</i>{' '}
-            </li>
-            <li>
-              <i>a mathematician's apology</i>{' '}
-            </li>
-            <li>
-              <i>guns, germs and steel</i>{' '}
-            </li>
-            <li>
-              <i>
-                <a href="http://www.incompleteideas.net/IncIdeas/BitterLesson.html">
-                  the bitter lesson
-                </a>
-              </i>{' '}
-            </li>
-            <li>
-              <i>
-                <a href="https://www.ben-evans.com/benedictevans/2020/5/16/not-even-wrong">
-                  predicting tech
-                </a>
-              </i>{' '}
-            </li>
-            <li>
-              <i>
-                <a href="https://danluu.com/look-stupid/">look stupid</a>
-              </i>{' '}
+              <a href="https://danluu.com/look-stupid/">look stupid</a>{' '}
             </li>
           </ul>
         </div>
