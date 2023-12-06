ecb933eece0b0005d6532e342af0a86ce392b5b2
Anya Singh <anyasingh070605@gmail.com>
2023-12-06 15:27:48 -0500

    change

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'home' },
-  { href: '/projects', title: 'projects' },
+  //{ href: '/projects', title: 'projects' },
   { href: '/bookshelf', title: 'bookshelf' },
   { href: '/about', title: 'about' },
   { href: 'https://substack.com/@anyasingh', title: 'substack' },
