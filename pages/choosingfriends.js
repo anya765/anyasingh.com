a1e24b9d574b2aad744f62049297072f926d19ba
Anya Singh <anyasingh070605@gmail.com>
2024-02-05 08:03:11 +0000

    update blog

--- pages/choosingfriends.js
+++ pages/choosingfriends.js
@@ -18,77 +18,87 @@ export default function choosingfriends() {
         </div>
         <div>
           <p>
-            i find it surprisingly common how many people, at least in my immediate circles chose
+            I find it surprisingly common how many people, at least in my immediate circles chose
             their friends based on the tangible output they create or the strength of their
             signalling. In school / work circles, people gravitate towards those who’ve worked at
             cool companies, or “built something cool”, or won a big award, etc.
-            <br />
+            <br /> <br />
             I’d argue that it makes for a good filter of ability / ambition to some extent (who
             doesn’t want accomplished friends) but not a good decider of who to genuinely spend time
             with (considering there’s likely hundreds of people in your circle who fit that
             description).
-            <br />
+            <br /> <br />
             I’d like to think that something as pertinant as who I spend time with on a regular
             would be thoroughly thought through and curated beyond just a resume and a few simple
-            words
+            words.
+            <br />
             <br />
             Realistically, you can only make time in your week for ~5 close friends, so what
             differentiates those who are close from those who aren’t. I’m somewhat surprised with
             the lack of attention to genuine, divergent personality traits given by people.
-            <br />
+            <br /> <br />
             While there’s nothing wrong with being very close friends with people who are, for
             example, good engineers, I’d argue that the role of friendship, or for that matter any
             human interaction is to grow as much as you can from it.
-            <br />
+            <br /> <br />
             As an example, simply being around good engineers, doesn’t directly make you able to
             produce better engineering work. Output, and work specifically doesn’t osmosize.
-            <br />
+            <br /> <br />
             Thoughtfulness and intelligence — I’ve found are something that while highly
             transferable aren’t particularly common based on someone’s pure ability.
-            <br />
+            <br /> <br />
             I think the question of friend filtering needs to be answered on a personal level.
-            <br />
+            <br /> <br />
             A good framework I’ve found for choosing good friends is, what traits does this person
             have that I can get through osmosis? What kind of people have the energy and personality
             that will rub off on me the more time I spend with them?
-            <br />
+            <br /> <br />
             What I started to realize is the majority of the things I cared about on a person level,
             were rarely fulfilled by the people who simply produced good work.
-            <br />
+            <br /> <br />
             Here’s a few rare personality traits I find myself both looking and optimizing for
             personally.
-            <br />
+            <br /> <br />
             - I believe genuine curiousity is something that is in incredibly short supply, people
-            that are genuinely curious should - someone who has moved around a lot is significantly
-            more likely to have contrarian views without actually realizing it - people who have
-            obsessive, yet unexpected interest are high signal because they’ve been immersed in a
-            world entirely different from the one they’re working in - focused people are rare - -
-            very rare. people that are ambitious, and make it a point to make fast progress without
-            timelines. - speed matters a lot (both in terms of interation and progress)- - some
-            people I’ve had the privelege of working with recently move at the speed of light. I
-            think the urgency of getting things done is something everyone should gravitate towards.
-            - people who give and seek feedback often - - especially to their closest friends tend
-            to be no bs, honest, growth focused, and just incredibly observant. - I’ve found that
-            people who’ve moved around a ton growing up have a much less sense of identity and
-            belonging - their ideas and interests are agile - uniquely optimistic people, or in
-            general people who default to interpreting situations in unexpected ways are more likely
-            to give you a new outlook on situations - i’ve yet to meet many risk taking - deep-end
-            type people - the type of people that commit to doing things way harder than anything
-            they’ve ever done before - - I think they’re willing - at least mentally - to struggle a
-            lot. - strongly principled people - - the people who say they’re not the type of person
-            to do certain things tend to have a strong sense of conviction about themselves/their
-            views, people who waver in principles / surprise you with the morality of their actions
-            aren’t the type of people I’d trust.
+            that are genuinely curious should be sought after because of their excitement for the
+            unknown <br />
+            - Someone who has moved around a lot is significantly more likely to have contrarian
+            views without actually realizing it <br />
+            - People who have obsessive, yet unexpected interest are high signal because they’ve
+            been immersed in a world entirely different from the one they’re working in. <br />
+            - focused people are rare - -very rare. people that are ambitious, and make it a point
+            to make fast progress without timelines. <br />
+            - speed matters a lot (both in terms of interation and progress)- - some people I’ve had
+            the privelege of working with recently move at the speed of light. I think the urgency
+            of getting things done is something everyone should gravitate towards.
             <br />
+            - people who give and seek feedback often - - especially to their closest friends tend
+            to be no bs, honest, growth focused, and just incredibly observant. <br />
+            - I’ve found that people who’ve moved around a ton growing up have a much less sense of
+            identity and belonging - their ideas and interests are agile <br />
+            - Uniquely optimistic people, or in general people who default to interpreting
+            situations in unexpected ways are more likely to give you a new outlook on situations{' '}
+            <br />
+            - I’ve yet to meet many risk taking - deep-end type people - the type of people that
+            commit to doing things way harder than anything they’ve ever done before - - I think
+            they’re willing - at least mentally - to struggle a lot. <br />
+            - strongly principled people - - the people who say they’re not the type of person to do
+            certain things tend to have a strong sense of conviction about themselves/ by extension
+            of letting people know about their strong opinion / views they subconsciously stop
+            themselves from changing their mind in the future.
+            <br />
+            <br /> <br />
             The people I spend time with are aggressively good at one thing. It’s a prerequisite to
             be ambitious and want to accomplish incredible things, but there’s more to consider
             about choosing the kinds of people to invest time with.
+            <br />
             <br />
             I tried to extrapolate on what I think is personally high signal and interesting in a
             potential friend. When I meet people that fit some criteria, I pursue closeness to them
             because I know I have a lot to gain from simply being around them. This was my attempt
             at trying to articulate this phenomenon. And hopefully encourage some discussion about
             what makes someone magnetic to be around.
+            <br />
             <br />
             People flock towards people who have similar interests as them (I guess it’s default to
             want to be around people like you), but in my view, those who find a way to be close to
